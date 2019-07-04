2ab9fb436aaf14754402e5756d6d63357dc0be74
Onaefe Edebi <onaefe.edebi@fidelitybank.ng>
2019-07-04 17:53:01 +0100

    Begin implementation of ajax submission

--- form_processor.php
+++ form_processor.php
@@ -16,6 +16,13 @@ if(isset($_POST["email_address"])){
     $email_address = $_POST["email_address"];
 }
 
+//==========MAIL SENDER DETAILS===============
+$to_address = "onaefee@gmail.com";
+$email_subject = "Emmanuel Consulting Guest Contact";
+$email_body = $message;
+mail($to_address,$email_subject,$email_body);
+//==========EEND OF MAIL SENDER DETAILS=======
+
 
 
 ?>
